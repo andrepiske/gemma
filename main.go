a49ee672032f90b9eb68beda179e7db589db5e53
André Diego Piske <andrepiske@gmail.com>
2022-01-20 23:15:45 +0100

    Add some code

--- main.go
+++ main.go
@@ -1,7 +1,93 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"strconv"
+)
+
+type Reader struct {
+	src *bufio.Reader
+}
+
+type RValue interface {}
+
+func NewReader(s io.Reader) *Reader {
+	return &Reader{src: bufio.NewReader(s)}
+}
+
+func (r *Reader) ReadValue() RValue {
+	p, _ := r.src.Peek(1)
+
+	if p[0] == '*' {
+		fmt.Println("will read an Array")
+		s, _ := r.src.ReadString('\n')
+
+		arrayLen, _ := strconv.Atoi(s[1:len(s)-2])
+
+		fmt.Printf("array has %d elements\n", arrayLen)
+
+    result := make([]RValue, 0)
+
+    for i := 0; i < arrayLen; i++ {
+      // read element of array
+      v := r.ReadValue()
+      result = append(result, v)
+    }
+
+    return result
+
+	} else if p[0] == '+' {
+		fmt.Println("will read a Simple String")
+
+		s, _ := r.src.ReadString('\n')
+    v := s[:len(s)-1]
+    fmt.Printf("Read simple string '%s'\n", v)
+
+    return v
+
+	} else if p[0] == '-' {
+		fmt.Println("will read an Error")
+		fmt.Println("not implemented")
+		panic(0)
+	} else if p[0] == ':' {
+		fmt.Println("will read an Integer")
+		fmt.Println("not implemented")
+		panic(0)
+	} else if p[0] == '$' {
+		fmt.Println("will read a complex String")
+		s, _ := r.src.ReadString('\n')
+		strLen, _ := strconv.Atoi(s[1:len(s)-2])
+    buf := make([]byte, strLen)
+    r.src.Read(buf)
+    r.src.ReadByte() // \r
+    r.src.ReadByte() // \n
+    v := string(buf)
+		fmt.Printf("string has length %d and it is '%s'\n", strLen, v)
+    return v
+	} else {
+		fmt.Printf("wuuuut is '%v'?\n", p)
+		panic(p)
+	}
+
+  return nil
+}
+
+func handleClient(conn net.Conn) {
+	r := NewReader(conn)
+	r.ReadValue()
+
+	conn.Close()
+}
 
 func main() {
-	fmt.Printf("hello world\n")
+	ln, _ := net.Listen("tcp", ":7111")
+
+	for {
+		fmt.Println("waiting new connection...")
+		conn, _ := ln.Accept()
+		go handleClient(conn)
+	}
 }
